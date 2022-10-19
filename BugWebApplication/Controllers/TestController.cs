214262fdd3bb7d41f5b40a92be354784bb612aed
Brandon Gulrich <brandon.gulrich@rev.com>
2022-10-19 09:52:08 -0500

    checkin

---
+++ BugWebApplication/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BugWebApplication.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet("/test/favicon.ico")]
+        public IActionResult Index()
+        {
+            return Ok("You shouldn't see me.");
+        }
+    }
+}
